b5de876e93752342a541be4c74e05a42db4d7d77
cptconundrum <conundrum@eprice.name>
2014-03-24 12:07:25 -0400

    Updated to v0.2.0

--- cSketchJS/ui/mods/cSketchJS/LavaDraw.js
+++ cSketchJS/ui/mods/cSketchJS/LavaDraw.js
@@ -1,10 +1,25 @@
-//LavaDraw is the lame name for a set of Sketch.js tools I'm writing for cptconundrum to use in this mod idea: https://forums.uberent.com/threads/request-click-drag-colorful-arrows-for-casters.58029/
+//------------ LavaDraw ------------
+	//Info: A set of Sketch.js tools by LavaSnake. LavaDraw was written for use by cptconundrum in the Sketch.js mod for PA casters.
+	//Version: 1.1
+	//URL: https://github.com/pamods/LavaSnake-s-Mods/tree/master/LavaDraw%20(Sketch.js%20Tools)
+	//Included Tools: "arrow" A basic arrow with an auto-added head, "stamp" An image stamp with a changeable image setting
 
+//Global LavaDraw Settings - Edit to change tool options
+var LavaDraw = {};
+LavaDraw.StampImg = "https://d3f1e1s5hz92ob.cloudfront.net/asset-version/z91a2e88bb4ecb89d84c97370febce7d9/Content/UberNetSite/images/img_item_detail_delta.png";
+
+//Basic arrow, uses global Sketch.js line settings
 $.sketch.tools.arrow = {
+	Xs: null,
+	Ys: null,
 	onEvent: function(e) {
 		switch (e.type) {
 			case 'mousedown':
 			case 'touchstart':
+				Xs = new Array();
+				Ys = new Array();
+				Xs.push(e.pageX - this.canvas.offset().left);
+				Ys.push(e.pageY - this.canvas.offset().top);
 				this.startPainting();
 				break;
 			case 'mouseup':
@@ -12,7 +27,69 @@ $.sketch.tools.arrow = {
 			case 'mouseleave':
 			case 'touchend':
 			case 'touchcancel':
-			this.stopPainting();
+				if (this.action) {
+					//Calculate arrow head direction
+					var x = Xs[Xs.length - 1];
+					var y = Ys[Ys.length - 1];
+					var oldX = x;
+					var oldY = y;
+					var count = Xs.length;
+					while (Math.abs(x - oldX) < 25 && Math.abs(y - oldY) < 25) {
+						count--;
+						if (count != 0) {
+							oldX = Xs[count];
+							oldY = Ys[count];
+						} else {
+							oldX = Xs[count];
+							oldY = Ys[count];
+							break;
+						}
+					}
+
+					var deltaX = x - oldX;
+					var deltaY = y - oldY;
+					var ArrowHead1X, ArrowHead1Y, ArrowHead2X, ArrowHead2Y;
+					if (Math.abs(deltaX) > Math.abs(deltaY)) {
+						//Line is horizontal
+						ArrowHead1Y =  -15;
+						ArrowHead2Y =  15;
+						if (deltaX > 0) {
+							//Line is left to right
+							ArrowHead1X = -15;
+							ArrowHead2X = -15;
+						} else {
+							//Line is right to left
+							ArrowHead1X = 15;
+							ArrowHead2X = 15;
+						}
+					} else {
+						//Line is vertical
+						ArrowHead1X = -15;
+						ArrowHead2X = +15;
+						if (deltaY > 0) {
+							//Line is top to bottom
+							ArrowHead1Y =  -15;
+							ArrowHead2Y =  -15;
+						} else {
+							//Line is bottom to top
+							ArrowHead1Y =  15;
+							ArrowHead2Y =  15;
+						}
+					}
+
+					//Add custom event to action with arrow head data
+					this.action.events.push({
+						x: x,
+						y: y,
+						ArrowHead1X: ArrowHead1X,
+						ArrowHead1Y: ArrowHead1Y,
+						ArrowHead2X: ArrowHead2X,
+						ArrowHead2Y: ArrowHead2Y,
+						event: "ArrowHead"
+					});
+				}
+
+				this.stopPainting();
 		}
 		if (this.painting) {
 			this.action.events.push({
@@ -20,6 +97,8 @@ $.sketch.tools.arrow = {
 				y: e.pageY - this.canvas.offset().top,
 				event: e.type
 			});
+			Xs.push(e.pageX - this.canvas.offset().left);
+			Ys.push(e.pageY - this.canvas.offset().top);
 			return this.redraw();
 		}
 	},
@@ -32,7 +111,19 @@ $.sketch.tools.arrow = {
 		_ref = action.events;
 		for (_i = 0, _len = _ref.length; _i < _len; _i++) {
 			event = _ref[_i];
-			this.context.lineTo(event.x, event.y);
+			if (event.event == "ArrowHead") {
+				//Render arrow head
+				this.context.moveTo(event.x + event.ArrowHead1X, event.y + event.ArrowHead1Y);
+				this.context.lineTo(event.x, event.y);
+
+				this.context.moveTo(event.x + event.ArrowHead2X, event.y + event.ArrowHead2Y);
+				this.context.lineTo(event.x, event.y);
+
+				this.context.moveTo(event.x, event.y);
+			} else {
+				//Draw line
+				this.context.lineTo(event.x, event.y);
+			}
 			previous = event;
 		}
 		this.context.strokeStyle = action.color;
@@ -41,52 +132,51 @@ $.sketch.tools.arrow = {
 	}
 };
 
+//Image Stamp, set the image used by the stamp in the LavaDraw object
+//This tool is by cptconundrum with improvements by LavaSnake
 $.sketch.tools.stamp = {
-	onEvent: function(e) {
-		switch (e.type) {
-			case 'mousedown':
-			case 'touchstart':
-				this.startPainting();
-				break;
-			case 'mouseup':
-			case 'mouseout':
-			case 'mouseleave':
-			case 'touchend':
-			case 'touchcancel':
-			this.stopPainting();
-		}
-		if (this.painting) {
-			this.action.events.push({
-				x: e.pageX - this.canvas.offset().left,
-				y: e.pageY - this.canvas.offset().top,
-				event: e.type
-			});
-			return this.redraw();
-		}
-	},
-	draw: function(action) {
-		var event, previous, _i, _len, _ref;
-		this.context.lineJoin = "round";
-		this.context.lineCap = "round";
-		this.context.beginPath();
-		this.context.moveTo(action.events[0].x, action.events[0].y);
-		_ref = action.events;
-		for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-			event = _ref[_i];
+    onEvent: function(e) {
+        switch (e.type) {
+            case 'mousedown':
+            case 'touchstart':
+                this.startPainting();
+                break;
+            case 'mouseup':
+            case 'mouseout':
+            case 'mouseleave':
+            case 'touchend':
+            case 'touchcancel':
+				this.stopPainting();
+        }
+        if (this.painting) {
+			//Save image action with current image setting
+            this.action.events.push({
+                x: e.pageX - this.canvas.offset().left,
+                y: e.pageY - this.canvas.offset().top,
+                event: e.type,
+				img: LavaDraw.StampImg
+            });
+            return this.redraw();
+        }
+    },
+    draw: function(action) {
+        var event, previous, _i, _len, _ref;
+        _ref = action.events;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            event = _ref[_i];
 
-			var canvas = document.getElementById('cSketchJS');
-			var context = canvas.getContext('2d');
-			var imageObj = new Image();
-			imageObj.onload = function() {
-				context.drawImage(imageObj, event.x, event.y);
-			};
-			imageObj.src = 'coui://ui/mods/cSketchJS/images/stamp_full.png';
+			//Draw image to canvas
+            var context = this.context;
+            var imageObj = new Image();
+            imageObj.onload = function() {
+				var OffsetX = imageObj.width / 2;
+				var OffsetY = imageObj.height / 2;
+                context.drawImage(imageObj, event.x - OffsetX, event.y - OffsetY);
+            };
+            imageObj.src = event.img;
 
-			previous = event;
-		}
-		this.context.strokeStyle = action.color;
-		this.context.lineWidth = action.size;
-		return this.context.stroke();
-	}
+            previous = event;
+        }
+        return this.context.stroke();
+    }
 };
-
--- cSketchJS/ui/mods/cSketchJS/cSketchJS.js
+++ cSketchJS/ui/mods/cSketchJS/cSketchJS.js
@@ -1,76 +1,128 @@
+/**
+ * cSketchJS
+ * By Captain Conundrum, LavaSnake, Martenus
+ *
+ * Adds a sketch.js layer over the game window so that casters can draw lines or arrows on the screen.
+ */
+
 var cSketchJS = (function () {
 
 	var cSketchJS = {};
 
+	/**
+	 * CONFIGURATION OPTIONS
+	 */
+
+	// These are the colors that show up in the toolbox.
 	cSketchJS.colors = [ '#05ABE0', '#d72828', '#d7d728', '#28d728' ];
 
+	// These options are sent to sketchJS when it is created.
 	cSketchJS.defaultOptions = {
 		defaultColor: cSketchJS.colors[0],
 		defaultTool: "marker",
 		toolLinks: true
 	};
 
+	// Options sent to rFloatFrame when the toolbox is created.
+	cSketchJS.floatingFrameOptions = {
+		"rememberPosition" : true,
+		"left":20,
+		"top":200
+	};
+
+	// Hold this key to draw
 	cSketchJS.drawButton = 18; // ALT
+
+	// Press this key to clear the canvas
+	cSketchJS.clearButton = 9; // TAB
+
+	// Override the default LavaDraw StampImg and set it here.
+	// The only reason to do this is just to keep all the configs in one place.
+	LavaDraw.StampImg = "https://d3f1e1s5hz92ob.cloudfront.net/asset-version/z91a2e88bb4ecb89d84c97370febce7d9/Content/UberNetSite/images/img_item_detail_delta.png";
+
+	/**
+	 * UTILITY FUNCTIONS
+	 */
 
 	cSketchJS.currentColor = function() { return $('#cSketchJS').sketch().color; };
 
 	cSketchJS.currentTool = function() { return $('#cSketchJS').sketch().tool; };
 
+	/**
+	 * MAIN cSketchJS CODE
+	 */
+
+	// Attach the drawing area to the top of the window and set up key listeners
 	cSketchJS.createCanvas = function() {
 		var _height = $('body').height();
 		var _width = $('body').width();
 
+		// Create a canvas that covers the whole screen.
+		// Set it to ignore all mouse events so that it doesn't interfere with the game.
 		$('body').prepend("<canvas id=\"cSketchJS\" class=\"ignoreMouse\" width=\"" + _width + "\" height=\"" + _height + "\"></canvas>");
+		// Tell sketchJS to control this canvas
 		$('#cSketchJS').sketch(cSketchJS.defaultOptions);
 
+		// Attach key listener
 		$('body').on("keydown", function(e) {
+			// Start drawing
 			if(e.which == cSketchJS.drawButton) {
 				$('#cSketchJS').removeClass("ignoreMouse");
 			}
+			// Clear the canvas
+			if(e.which == cSketchJS.clearButton) {
+				cSketchJS.clearSketch();
+			}
 		});
 
+		// Attach listener
 		$('body').on("keyup", function(e) {
+			// Stop drawing
 			if(e.which == cSketchJS.drawButton) {
 				$('#cSketchJS').addClass("ignoreMouse");
 			}
 		});
 	};
 
+	// Set up the UI
 	cSketchJS.createToolbox = function() {
 
+		// Helper functions for Hex to RGB
 		// http://www.javascripter.net/faq/hextorgb.htm
 		function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
 		function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
 		function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
 		function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
-		createFloatingFrame("cSketchJS_toolbox", 45, 155, {
-			"rememberPosition" : false,
-			"left":20,
-			"top":200
-		});
+		// Use rFloatFrame to make the toolbox draggable
+		createFloatingFrame("cSketchJS_toolbox", 45, 155, cSketchJS.floatingFrameOptions);
 
-
+		// Add the colors to the toolbox
 		$.each(cSketchJS.colors, function(_index, _color) {
 			var rgbColor = [];
 
+			// Draw color
 			rgbColor[0] = hexToR(_color);
 			rgbColor[1] = hexToG(_color);
 			rgbColor[2] = hexToB(_color);
 
 			var lightColor = [];
 
+			// Lighter color. This is for the gradient on the button
 			lightColor[0] = Math.floor((255 - rgbColor[0]) / 2 + rgbColor[0]);
 			lightColor[1] = Math.floor((255 - rgbColor[1]) / 2 + rgbColor[1]);
 			lightColor[2] = Math.floor((255 - rgbColor[2]) / 2 + rgbColor[2]);
 
+			// Create the gradient
 			var _background = "linear-gradient(135deg, rgba(" + lightColor[0] + "," + lightColor[1] + "," + lightColor[2] + ",1) 0%,rgba(" + rgbColor[0] + "," + rgbColor[1] + "," + rgbColor[2] + ",1) 100%)";
 
+			// Attach the color button
 			$('#cSketchJS_toolbox_content').append("<a href='#cSketchJS' class=\"color\" data-color='" + _color + "' style='background: " + _background + ";'></a> ");
 		});
 
 		$('#cSketchJS_toolbox_content').append("<hr />");
 
+		// Attach the tools
 		$('#cSketchJS_toolbox_content').append("<a id=\"cSketchJS_line\" class=\"tool\" href='#cSketchJS' data-tool=\"marker\"></a> ");
 		$('#cSketchJS_toolbox_content').append("<a id=\"cSketchJS_arrow\" class=\"tool\" href='#cSketchJS' data-tool=\"arrow\"></a> ");
 		$('#cSketchJS_toolbox_content').append("<a id=\"cSketchJS_stamp\" class=\"tool\" href='#cSketchJS' data-tool=\"stamp\"></a> ");
@@ -79,20 +131,24 @@ var cSketchJS = (function () {
 
 		$('#cSketchJS_toolbox_content').append("<a id=\"cSketchJS_clear\" href=\"javascript:cSketchJS.clearSketch();\"></a> ");
 
+		// Attach "click" listener
 		$("#cSketchJS_toolbox_content a.color").on("click", function() {
 			$("#cSketchJS_toolbox_content a.color").removeClass("selected");
 			$(this).addClass("selected");
 		});
 
+		// Attach "click" listener
 		$("#cSketchJS_toolbox_content a.tool").on("click", function() {
 			$("#cSketchJS_toolbox_content a.tool").removeClass("selected");
 			$(this).addClass("selected");
 		});
 
+		// Mark the first color and first tool as selected
 		$("#cSketchJS_toolbox_content a.color:first").addClass("selected");
 		$("#cSketchJS_toolbox_content a.tool:first").addClass("selected");
 	};
 
+	// Replace the canvas with a clean one
 	cSketchJS.clearSketch = function() {
 		var currentColor = cSketchJS.currentColor();
 		cSketchJS.defaultOptions.defaultColor = currentColor;
@@ -105,6 +161,7 @@ var cSketchJS = (function () {
 	return cSketchJS;
 })();
 
+// Initialize cSketchJS
 (function() {
 	cSketchJS.createCanvas();
 	cSketchJS.createToolbox();
