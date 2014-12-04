b9fac997d0f41066ba30a1aea8cf4fa90b6639ec
cptconundrum <conundrum@eprice.name>
2014-12-03 20:28:27 -0500

    Added Gandalf's map pack

---
+++ cMaps_gandalf/ui/mods/cMaps_gandalf/maplist.js
@@ -0,0 +1,7 @@
+cShareSystems.load_pas("Gandalf", [
+	"coui://ui/mods/cMaps_gandalf/systems/1v1_arena.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/canyon_wars.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/iceage.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/prof_x.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/team_arena.pas"
+]);
