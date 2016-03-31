83c2600c74e944a020951ced31d7373dea9d354c
cptconundrum <conundrum@eprice.name>
2016-03-31 10:58:49 -0400

    exodus and infinite build update

--- cExodus/ui/mods/cExodus/videofeed/videofeed.js
+++ cExodus/ui/mods/cExodus/videofeed/videofeed.js
@@ -13,8 +13,8 @@
 	}
 	model.communityTabGroup('exodusvideos');
 
-	$('.tab_content').prepend('<div class="full_update" data-bind="visible: showExodusVideos"><div class="section_header">eXodus Videos</div><div id="exodusvideo"><div class="ytv-wrapper" id="cExodus_ytv-wrapper"><div class="null-msg"><loc>Video list could not be retrieved.</loc></div></div></div></div>'); 
-	$('.tab_controls').prepend('<div class="community_tab btn_std_ix" data-placement="top" data-bind="click: toggleExodusVideosTab, click_sound: \'default\', rollover_sound: \'default\', css: { \'active\': $root.showExodusVideos() }, tooltip: \'eXodus Videos\'"><img src="coui://ui/mods/cExodus/img/logo_small.png" width="32"/></div>')
+	$('.tab_content').prepend('<div class="full_update" data-bind="visible: showExodusVideos"><div class="section_header">eXodus Videos</div><div id="exodusvideo"><div class="ytv-wrapper" id="cExodus_ytv-wrapper"><div class="null-msg"><loc>Video list could not be retrieved.</loc></div></div></div></div>');
+	$('.tab_controls').prepend('<div class="community_tab btn_std_ix" data-placement="top" data-bind="click: toggleExodusVideosTab, click_sound: \'default\', rollover_sound: \'default\', css: { \'active\': $root.showExodusVideos() }, tooltip: \'eXodus Videos\'"><img src="coui://ui/mods/cexodus/img/logo_small.png" width="32"/></div>')
 
 	// Load videos
     $("#cExodus_ytv-wrapper").ytv({
